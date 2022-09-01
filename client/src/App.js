547f9d58b7c5508dec5b0aaeea202515073e1a13
justinn79 <justinlylemiraflor@gmail.com>
2022-09-01 17:24:59 -0400

    form sends info to db

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import { getPosts } from './actions/posts';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+
+import useStyles from './styles';
 
 function App() {
+
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Testerfdsf
-        </p>
-      </header>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Appname</Typography>
+        
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+            
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
---
+++ client/src/reducers/posts.js
@@ -0,0 +1,10 @@
+export default (posts = [], action) => {
+    switch(action.type) {
+        case 'FETCH_ALL':
+            return action.payload;
+        case 'CREATE':
+            return [...action, action.payload];
+        default:
+            return posts;
+    }
+}
